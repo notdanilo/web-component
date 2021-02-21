a297086f9d998ac23fdbfcce38224e52b645cbb4
Danilo Guanabara <danguafer@gmail.com>
2021-02-21 11:15:05 -0300

    Online debugging #2

--- web-component-loader.js
+++ web-component-loader.js
@@ -21,6 +21,7 @@ export default class WebComponentLoader extends HTMLElement {
 
     async load() {
         let path   = this.getModulePath();
+        console.log(path);
         let module = await import(path);
         let loader = this.createLoader(module);
         await loader.load(module);
